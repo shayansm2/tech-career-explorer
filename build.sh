1cfbd9719cbfc0ab184d21cde14b5f14b9dd441b
shayan <shayansm09@gmail.com>
2024-03-15 00:44:23 +0330

    fix some of build and configs

--- build.sh
+++ build.sh
@@ -2,12 +2,11 @@
 
 if [ ! -f ".env" ]; then
     touch .env
-    echo PROJECT_NAME=tech-job-crawler >> .env
     echo POSTGRES_DBNAME=tech_jobs >> .env
     echo POSTGRES_SCHEMA=public >> .env
     echo POSTGRES_USER=user >> .env
     echo POSTGRES_PASSWORD=password >> .env
-    echo POSTGRES_HOST=localhost >> .env
+    echo POSTGRES_HOST=postgres >> .env
     echo POSTGRES_PORT=5432 >> .env
 fi
 
